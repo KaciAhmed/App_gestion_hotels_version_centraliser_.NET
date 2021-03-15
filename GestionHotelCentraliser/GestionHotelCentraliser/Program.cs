43e6c1316f78d82491ca36e315ce636ea8603591
ahmedBylka <kaciahmed3@gmail.com>
2021-03-15 11:25:34 +0100

    Premier commit

---
+++ GestionHotelCentraliser/GestionHotelCentraliser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestionHotelCentraliser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
